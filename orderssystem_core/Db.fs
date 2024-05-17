865d7f9e27e2030c7a1c02f85adbd9acf4df1adf
Tonino Lucca <tonyx1@gmail.com>
2024-05-17 06:12:09 +0200

    asldkj

--- orderssystem_core/Db.fs
+++ orderssystem_core/Db.fs
@@ -17,6 +17,8 @@ let [<Literal>] dbVendor = Common.DatabaseProviderTypes.POSTGRESQL
 let [<Literal>] resPath = "" 
 let [<Literal>] indivAmount = 1000
 let [<Literal>] useOptTypes  = false
+// let [<Literal>] nullable = Common.NullableColumnType.NO_OPTION //  = false
+// let [<Literal>] nullable = Common.NullableColumnType.NO_OPTION
 
 type Sql =
     SqlDataProvider< 
